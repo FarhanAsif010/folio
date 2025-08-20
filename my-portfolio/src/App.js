e096f29cccf06118e430e9dace0eb745471ac58e
FarhanAsif010 <khawajafarhan010@gmail.com>
2025-08-20 19:02:05 +0500

    Import BrowserRouter as HashRouter and changed package.json

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,5 +1,5 @@
 import React, { useRef } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header.js';
 import Portfolio from './portfolio.js';
 import AboutMe from './components/AboutMe.js';
